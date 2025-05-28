44c3f3b6638717c20192ebde2d56294ecf5b41d4
Mohannad <mohannad.karout1986@gmail.com>
2025-05-28 08:57:30 +0300

    تحديث Seeder لإضافة المنتجات الجديدة

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
-use App\models\Category;
+use App\Models\Category;
 
 class CategoryTableSeeder extends Seeder
 {
